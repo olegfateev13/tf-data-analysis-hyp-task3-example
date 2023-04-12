4cd89550e31f0991ced4138d3f377b28784fb3e2
olegfateev13 <58751132+olegfateev13@users.noreply.github.com>
2023-04-12 19:29:40 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,15 @@
+import scipy.stats as stats
 import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 263008738 
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(sample) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+    t_stat, p_value = stats.ttest_1samp(sample, 500)
+    alpha = 0.06
+    if p_value < alpha:
+        reject_null_hypothesis = True
+    else:
+        reject_null_hypothesis = False
+    return reject_null_hypothesis # Ваш ответ, True или False
